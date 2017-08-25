368c87bbe19f0d35ef9baa689efbb90aba5d8b7f
lixu <lixu_sx@qiyi.com>
2017-08-25 10:52:47 +0800

    add file

---
+++ RNVIPCenter/RNVIPCenter.h
@@ -0,0 +1,13 @@
+//
+//  RNVIPCenter.h
+//  RNVIPCenter
+//
+//  Created by iqiyi on 17/8/25.
+//  Copyright © 2017年 iqiyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNVIPCenter : NSObject
+
+@end
---
+++ RNVIPCenter/RNVIPCenter.m
@@ -0,0 +1,13 @@
+//
+//  RNVIPCenter.m
+//  RNVIPCenter
+//
+//  Created by iqiyi on 17/8/25.
+//  Copyright © 2017年 iqiyi. All rights reserved.
+//
+
+#import "RNVIPCenter.h"
+
+@implementation RNVIPCenter
+
+@end
